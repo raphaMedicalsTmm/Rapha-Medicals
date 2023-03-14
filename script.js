f8fc53ee788dd389021d056bf3063f1a5706f7b3
raphaMedicalsTmm <124604058+raphaMedicalsTmm@users.noreply.github.com>
2023-03-15 02:33:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function wp(){
 
 // form script
 
-const scriptURL = 'https://script.google.com/macros/s/AKfycbzS0bkvATIR_UwpHF4V31uRFEa4Jr63EfDqsry4nh-aaoqbWNq0oZdBxqyAiFs7HCRWtQ/exec';
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyRebVwMUTsX48nURlipSrN1r_HNeWTlCjmjCyr2IZuSPJ1mQiWqLuBBhLGFVnD7GZ6_A/exec';
 let url = "https://script.google.com/macros/s/AKfycbwrFdZYKgTtktrLxzUXsrR97oA9K27o-q8HFdFzFbIUGu7wjTxPPce7o3jTslUHyZ--XA/exec"; //url of the seperate project(https://script.google.com/home/projects/1s74sqraE-Ctf0GrGydq6Ipb6AYCkkZ7566HXLSOj1l0zvyj5_eBhjdRE/edit)
 
 
