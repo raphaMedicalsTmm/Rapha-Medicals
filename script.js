c7203a9a52207e1a8f0108f939449d82f9c78073
raphaMedicalsTmm <124604058+raphaMedicalsTmm@users.noreply.github.com>
2023-03-25 23:50:22 +0530

    timestamp update

--- script.js
+++ script.js
@@ -17,8 +17,8 @@ function wp(){
 
 // form script
 
-const scriptURL = 'https://script.google.com/macros/s/AKfycbyRebVwMUTsX48nURlipSrN1r_HNeWTlCjmjCyr2IZuSPJ1mQiWqLuBBhLGFVnD7GZ6_A/exec';
-let url = "https://script.google.com/macros/s/AKfycbwrFdZYKgTtktrLxzUXsrR97oA9K27o-q8HFdFzFbIUGu7wjTxPPce7o3jTslUHyZ--XA/exec"; //url of the seperate project(https://script.google.com/home/projects/1s74sqraE-Ctf0GrGydq6Ipb6AYCkkZ7566HXLSOj1l0zvyj5_eBhjdRE/edit)
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzvKAP9OWGkThWc8IPnUSUyLy7cT6Dmc6v0riTfdOUJrkoC5OClTmo0HATUScNjx7d2Pw/exec';
+let url = "https://script.google.com/macros/s/AKfycbwrFdZYKgTtktrLxzUXsrR97oA9K27o-q8HFdFzFbIUGu7wjTxPPce7o3jTslUHyZ--XA/exec"; //url of the seperate app script project(https://script.google.com/home/projects/1s74sqraE-Ctf0GrGydq6Ipb6AYCkkZ7566HXLSOj1l0zvyj5_eBhjdRE/edit)
 
 
 let form = document.forms['submit-to-google-sheet'];
